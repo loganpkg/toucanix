1383dbbe24862d0339f2cf86dc345a50d4bb51cc
Logan McLintock <loganpkg@gmail.com>
2025-01-08 22:05:22 +1030

    enter protected mode

--- loader.asm
+++ loader.asm
@@ -29,6 +29,72 @@ kernel_start_sector equ MBR_SECTOR + NUM_OF_LOADER_SECTORS_TO_READ
 %define NUM_OF_KERNEL_SECTORS_TO_READ 120
 
 
+%define PRESENT_BIT_SET                   (1 << 7)
+%define DESCRIPTOR_PRIVILEGE_LEVEL_KERNEL (0 << 5)
+%define DESCRIPTOR_PRIVILEGE_LEVEL_USER   (3 << 5)
+%define TYPE_IS_CODE_OR_DATA_SEGMENT      (1 << 4)
+%define EXECUTABLE_FOR_CODE               (1 << 3)
+%define NON_EXECUTABLE_FOR_DATA           (0 << 3)
+%define CODE_RING_IN_DPL_ONLY             (0 << 2)
+%define DATA_DIRECTION_GROWS_UP           (0 << 2)
+%define CODE_READ_ACCESS                  (1 << 1)
+%define DATA_WRITE_ACCESS                 (1 << 1)
+%define ACCESSED_BIT_CLEAR                (0 << 0)
+
+code_access_byte equ PRESENT_BIT_SET    \
+    | DESCRIPTOR_PRIVILEGE_LEVEL_KERNEL \
+    | TYPE_IS_CODE_OR_DATA_SEGMENT      \
+    | EXECUTABLE_FOR_CODE               \
+    | CODE_RING_IN_DPL_ONLY             \
+    | CODE_READ_ACCESS                  \
+    | ACCESSED_BIT_CLEAR
+
+data_access_byte equ PRESENT_BIT_SET    \
+    | DESCRIPTOR_PRIVILEGE_LEVEL_KERNEL \
+    | TYPE_IS_CODE_OR_DATA_SEGMENT      \
+    | NON_EXECUTABLE_FOR_DATA           \
+    | DATA_DIRECTION_GROWS_UP           \
+    | DATA_WRITE_ACCESS                 \
+    | ACCESSED_BIT_CLEAR
+
+%define GRANULARITY_4_KIB   (1 << 3)
+%define SIZE_32_BIT_SEGMENT (1 << 2)
+%define NOT_LONG_MODE_CODE  (0 << 1)
+%define RESERVED_FLAG       (0 << 0)
+
+flags_nibble equ GRANULARITY_4_KIB \
+    | SIZE_32_BIT_SEGMENT          \
+    | NOT_LONG_MODE_CODE           \
+    | RESERVED_FLAG
+
+%define BASE_ADDRESS 0
+; %define BASE_ADDRESS 0xefcdab89
+
+%define SEGMENT_LIMIT 0xfffff
+
+
+%define NULL_SEGMENT 0
+%define CODE_SEGMENT_INDEX 1
+%define DATA_SEGMENT_INDEX 2
+
+%define TABLE_INDICATOR_GDT (0 << 2)
+%define REQUESTOR_PRIVILEGE_LEVEL_KERNEL 00b
+
+code_selector equ CODE_SEGMENT_INDEX << 3 \
+    | TABLE_INDICATOR_GDT                 \
+    | REQUESTOR_PRIVILEGE_LEVEL_KERNEL
+
+data_selector equ DATA_SEGMENT_INDEX << 3 \
+    | TABLE_INDICATOR_GDT                 \
+    | REQUESTOR_PRIVILEGE_LEVEL_KERNEL
+
+
+%define IDT_INVALID_SIZE_MINUS_1 0
+%define INTERRUPT_DESCRIPTOR_TABLE_INVALID_ADDRESS 0
+
+%define PROTECTED_MODE 1
+
+
 [BITS 16]
 [ORG loader_address]
 
@@ -71,6 +137,32 @@ xor di, di
 mov byte [es:di], 'K'
 mov byte [es:di + 1], green_on_black
 
+cli
+lgdt [GDT_descriptor]
+lidt [IDT_descriptor]
+
+mov eax, cr0
+or eax, PROTECTED_MODE
+mov cr0, eax
+
+
+jmp code_selector:protected_mode_start
+
+
+[BITS 32]
+protected_mode_start:
+
+mov ax, data_selector
+mov ds, ax
+mov es, ax
+mov ss, ax
+
+mov esp, MBR_ADDRESS
+
+mov byte [VIDEO_ADDRESS], 'P'
+mov byte [VIDEO_ADDRESS + 1], grey_on_black
+
+
 done:
 hlt
 jmp done
@@ -83,3 +175,37 @@ db 0
 dw NUM_OF_KERNEL_SECTORS_TO_READ
 dw kernel_offset, kernel_segment
 dq kernel_start_sector
+
+
+
+global_descriptor_table:
+dq NULL_SEGMENT
+
+; Code segment.
+dw SEGMENT_LIMIT & 0xffff
+dw BASE_ADDRESS & 0xffff
+db BASE_ADDRESS >> 16 & 0xff
+db code_access_byte
+db flags_nibble << 4 | SEGMENT_LIMIT >> 16
+db BASE_ADDRESS >> 24
+
+; Data segment.
+dw SEGMENT_LIMIT & 0xffff
+dw BASE_ADDRESS & 0xffff
+db BASE_ADDRESS >> 16 & 0xff
+db data_access_byte
+db flags_nibble << 4 | SEGMENT_LIMIT >> 16
+db BASE_ADDRESS >> 24
+
+
+GDT_size_minus_1 equ $ - global_descriptor_table - 1
+
+
+GDT_descriptor:
+dw GDT_size_minus_1
+dd global_descriptor_table
+
+
+IDT_descriptor:
+dw IDT_INVALID_SIZE_MINUS_1
+dd INTERRUPT_DESCRIPTOR_TABLE_INVALID_ADDRESS
